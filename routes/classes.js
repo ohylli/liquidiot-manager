9a44035b91fe4df549929296efd779baff5f0474
Otto Hylli <otto.hylli@tut.fi>
2016-03-01 10:00:40 +0200

    added copyright notice

--- routes/classes.js
+++ routes/classes.js
@@ -1,8 +1,22 @@
+/**
+ * Copyright (c) TUT Tampere University of Technology 2015-2016
+ * All rights reserved.
+ *
+ * Main author(s):
+ * Otto Hylli <otto.hylli@tut.fi>
+*/
+
 var express = require( 'express' );
+
 var router = express.Router();
 
 router.get( '/', function ( req, res ) {
     res.send( { message: 'joo' } );
+});
+
+router.put( "/:class", function( req, res ) {
+    var db = req.db;
+    res.send();
 });
 
 module.exports = router;
