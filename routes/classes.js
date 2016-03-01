44dde93298c3aed7cc0f906e8ed98f44c37335a6
Otto Hylli <otto.hylli@tut.fi>
2016-03-01 11:31:17 +0200

    implemented api for creating / updating classes

--- routes/classes.js
+++ routes/classes.js
@@ -14,9 +14,21 @@ router.get( '/', function ( req, res ) {
     res.send( { message: 'joo' } );
 });
 
+// create or update a class api description
 router.put( "/:class", function( req, res ) {
     var db = req.db;
-    res.send();
+    var query = { 'name': req.params.class };
+    // object to be added
+    var api = { name: req.params.class, api: JSON.stringify( req.body ) };
+    // option for inserting a document if none match query
+    var options = { upsert: true };
+    db.collection( 'classes' ).updateOne( query, api, options, function ( err, result ) { 
+        if ( err ) {
+            return res.status( 500 ).send( err );
+        }
+
+        res.send( req.body );
+    });
 });
 
 module.exports = router;
